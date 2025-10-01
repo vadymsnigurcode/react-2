402ccb2ff3261f547c253113d6692849cca28acf
Vadym <vadym.snigur.code@gmail.com>
2025-10-01 21:43:44 +0300

    logical conditional message.

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -2,16 +2,10 @@ function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris", "Kyiv"];
   items = [];
 
-  const getMessage = () => {
-
-    return items.length === 0 ? <p>No items found</p> : null;
-
-  }
-
   return (
     <>
       <h1>List</h1>
-      {getMessage()}
+      {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
