7323b2428a0a4760bea9ea46860f89a464ab9873
Vadym <vadym.snigur.code@gmail.com>
2025-10-01 22:41:34 +0300

    select item from list group using hook

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,14 +1,33 @@
+import { useState } from "react";
+
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris", "Kyiv"];
-//   items = [];
 
+  // Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+
+  //   items = [];
+
+  // event handler
+  const handleClick = (event: MouseEvent) => console.log(event);
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li className = "list-group-item" key={item} onClick={()=> console.log(item)}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={()=>{setSelectedIndex( index)}}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
